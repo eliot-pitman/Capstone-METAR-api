373b51b8d56871405dc7071bbf64694719604447
Eliot <eliotpitman@gmail.com>
2022-04-18 15:38:28 -0500

    removes airport create

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -1,15 +1,25 @@
 class AirportsController < ApplicationController
-  def create
-    airport = Airport.new(
-      iata: params[:iata],
-      icao: params[:icao],
-      name: params[:name],
-      location: params[:location]
-    )
-    if airport.save
-      render json: airport.as_json
-    else
-      render json: { errors: airport.errors.full_messages }, status: :bad_request
-    end
-  end 
+  before_action ::authenticate_user
+  require 'http'
+  # def create
+  #   airport = Airport.new(
+  #     iata: params[:iata],
+  #     icao: params[:icao],
+  #     name: params[:name],
+  #     location: params[:location]
+  #   )
+  #   if airport.save
+  #     render json: airport.as_json
+  #   else
+  #     render json: { errors: airport.errors.full_messages }, status: :bad_request
+  #   end
+  # end 
+
+  # def index
+
+  #   user = USer.find(current_user.id)
+  #   user.favorites
+  #   resposne = HTTP.get()
+    
+  # end
 end
